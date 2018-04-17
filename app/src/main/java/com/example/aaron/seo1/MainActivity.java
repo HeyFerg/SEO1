8796b7b7a676afa5a1cbe1361f52a9addc6adbc6
Aaron <2ferga42@solent.ac.uk>
2018-04-17 22:14:47 +0100

    Completed Activity 1 of the assignment.

--- app/src/main/java/com/example/aaron/seo1/MainActivity.java
+++ app/src/main/java/com/example/aaron/seo1/MainActivity.java
@@ -9,13 +9,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.app.Activity;
+import android.location.LocationManager;
+import android.location.LocationListener;
+import android.location.Location;
+import android.content.Context;
+import android.widget.Toast;
+
 
 //import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity implements LocationListener
 {
     MapView mv;
     @Override
@@ -26,8 +33,36 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        LocationManager mgr=(LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        mgr.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,this);
+
         mv = (MapView)findViewById(R.id.map1);
+        mv.setBuiltInZoomControls(true);
+        mv.getController().setZoom(16);
+
     }
+
+    public void onLocationChanged(Location newLoc)
+    {
+        Toast.makeText(this, "Location=" + newLoc.getLatitude()+ "" + newLoc.getLongitude(), Toast.LENGTH_LONG).show();
+        mv.getController().setCenter(new GeoPoint(newLoc));
+    }
+
+    public void onProviderDisabled(String provider)
+    {
+        Toast.makeText(this, "Provider" + provider + "disabled", Toast.LENGTH_LONG).show();
+    }
+
+    public void onProviderEnabled(String provider)
+    {
+        Toast.makeText(this, "Provider" + provider + "enabled", Toast.LENGTH_LONG).show();
+    }
+
+    public void onStatusChanged(String provider, int status, Bundle extras)
+    {
+        Toast.makeText(this, "Status changed:" + status, Toast.LENGTH_LONG).show();
+    }
+
 
     public boolean onCreateOptionsMenu(Menu menu)
     {
@@ -61,5 +96,6 @@ public class MainActivity extends AppCompatActivity
 
             return true;
         }
+        return false;
     }
 }
